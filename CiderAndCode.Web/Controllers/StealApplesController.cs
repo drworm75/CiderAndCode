131330becf79c3c4011947481fd4f3da4bd78a34
unknown <dwgames@gmail.com>
2017-09-25 17:08:48 -0700

    setting up new route

---
+++ CiderAndCode.Web/Controllers/StealApplesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace CiderAndCode.Web.Controllers
+{
+    [RoutePrefix("api/StealApples")]
+    public class StealApplesController : ApiController
+    {
+        /*
+        [Route(""), HttpPost]
+        public HttpResponseMessage StoleDemApples(ApplesStolenRequest request)
+        {
+            var db = new AppDbContext();
+
+            var deleteBushel = new BushelController
+            {
+
+            };
+
+            db.Bushels.Delete(deleteBushel);
+            db.SaveChanges();
+
+            return Request.CreateResponse(HttpStatusCode.Moved, deleteBushel);
+
+        }
+        */
+    }
+}
--- CiderAndCode.Web/app/appconfig.js
+++ CiderAndCode.Web/app/appconfig.js
@@ -1,8 +1,15 @@
 ﻿app.config(["$routeProvider",function($routeProvider) {
     $routeProvider
         .when("/pickapples",
-            {
+        {
                 templateUrl: "/app/views/PickApples/pickApples.html",
                 controller: "pickApplesController"
-            });
+        })
+        .when("/stealapples",
+        {
+            templateUrl: "/app/views/StealApples/stealApples.html",
+            controller: "stealApplesController"
+
+        })
+        ;
 }]);
